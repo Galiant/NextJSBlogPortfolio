cc662a44b38e375b8dd0e37dc0f7365056890418
Antonijo Galic <antonijodev@gmail.com>
2024-06-08 09:51:50 +0200

    Site header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'Antonijo Galic',
+  url: 'https://antonijogalic.com/',
+  description: 'Personal portfolio and blog page',
+  author: 'Antonijo Galic',
+  links: {
+    linkedin: 'https://www.linkedin.com/in/antonijogalic/',
+    github: 'https://github.com/Galiant',
+    twitter: 'https://twitter.com/antonijogalic',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
